be9bc5bfc583bc75f58038ed282a191d07d66701
GustavoAOliveira <gustavoaquino001@hotmail.com>
2024-01-11 11:49:23 -0300

    Initial Structure

---
+++ Name-Spring-Security-studies/src/main/java/com/example/Name/Spring/Security/studies/NameSpringSecurityStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Name.Spring.Security.studies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameSpringSecurityStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameSpringSecurityStudiesApplication.class, args);
+	}
+
+}
---
+++ Name-Spring-Security-studies/src/test/java/com/example/Name/Spring/Security/studies/NameSpringSecurityStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Name.Spring.Security.studies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameSpringSecurityStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
